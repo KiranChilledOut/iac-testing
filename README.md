8f46ec660d8a0d662aede5b0d8288e27634eac96
Kiran Raj Rajendran <krajendran@tesla.com>
2024-03-29 18:20:39 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Terraform AWS
+
+## Local state
+
+- Create `main.tf`
---
+++ main.tf
@@ -0,0 +1,32 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = var.region
+}
+
+data "aws_ami" "example" {
+  most_recent      = true
+
+  filter {
+    name   = "name"
+    values = ["myami-*"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
