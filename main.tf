e48d1bb5eb4ed4a584164a4bca13c18dc1e1e96a
citadelforsomerefuge@gmail.com <citadelforsomerefuge@gmail.com>
2024-03-29 21:03:14 +0100

    Commit code for deployment

--- main.tf
+++ main.tf
@@ -17,16 +17,37 @@ data "aws_ami" "example" {
 
   filter {
     name   = "name"
-    values = ["myami-*"]
-  }
-
-  filter {
-    name   = "root-device-type"
-    values = ["ebs"]
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
   }
 
   filter {
     name   = "virtualization-type"
     values = ["hvm"]
   }
+
+  owners = [099720109477]
+}
+
+resource "aws_key_pair" "sample_key" {
+  key_name   = "sample-key"
+  public_key = var.sample_public_key
+
+  tags = {
+    "Name" = "sample_public_key"
+  }
+}
+
+resource "aws_instance" "sample_server" {
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = ["sg-0d56146975407f3da"]
+  key_name               = aws_key_pair.sample_key.key_name
+
+  tags = {
+    "Name" = "sample_server"
+  }
+}
+
+output "sample_server_dns" {
+  value = aws_instance.sample_server.public_dns
 }
