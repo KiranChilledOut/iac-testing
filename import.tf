0503c318cc41998da4a4276a807e1b4ba864b046
citadelforsomerefuge@gmail.com <citadelforsomerefuge@gmail.com>
2024-03-30 01:34:09 +0100

    Adding after Testing

---
+++ import.tf
@@ -0,0 +1,9 @@
+# import {
+#   id = "i-04a053c8cd9dc6f9e"
+#   to = aws_instance.this
+# }
+
+# import {
+#   id = "sg-0b7a00db5bbbf8a0a"
+#   to = aws_security_group.this
+# }
--- main.tf
+++ main.tf
@@ -1,33 +1,7 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
+# __generated__ by Terraform
+# Please review these resources and move them into your main configuration files.
 
-# Configure the AWS Provider
-provider "aws" {
-  region = var.region
-}
-
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
-}
-
+# __generated__ by Terraform from "sg-0b7a00db5bbbf8a0a"
 resource "aws_key_pair" "sample_key" {
   key_name   = "sample-key"
   public_key = var.sample_public_key
@@ -36,18 +10,58 @@ resource "aws_key_pair" "sample_key" {
     "Name" = "sample_public_key"
   }
 }
+resource "aws_security_group" "this" {
+  description = "ssh and https"
+  egress = [{
+    cidr_blocks      = ["0.0.0.0/0"]
+    description      = ""
+    from_port        = 0
+    ipv6_cidr_blocks = []
+    prefix_list_ids  = []
+    protocol         = "-1"
+    security_groups  = []
+    self             = false
+    to_port          = 0
+  }]
+  ingress = [{
+    cidr_blocks      = ["0.0.0.0/0"]
+    description      = ""
+    from_port        = 22
+    ipv6_cidr_blocks = []
+    prefix_list_ids  = []
+    protocol         = "tcp"
+    security_groups  = []
+    self             = false
+    to_port          = 22
+    }, {
+    cidr_blocks      = ["0.0.0.0/0"]
+    description      = ""
+    from_port        = 443
+    ipv6_cidr_blocks = []
+    prefix_list_ids  = []
+    protocol         = "tcp"
+    security_groups  = []
+    self             = false
+    to_port          = 443
+  }]
+  name                   = "ssh-https"
+  name_prefix            = null
+  revoke_rules_on_delete = null
+  tags                   = {}
+  tags_all               = {}
+  vpc_id                 = "vpc-01c0cd4d7700ba67f"
+}
 
-resource "aws_instance" "sample_server" {
-  ami                    = data.aws_ami.ubuntu.id
-  instance_type          = "t2.micro"
-  vpc_security_group_ids = ["sg-0d56146975407f3da"]
-  key_name               = aws_key_pair.sample_key.key_name
-
-  tags = {
-    "Name" = "sample_server"
-  }
+# __generated__ by Terraform
+resource "aws_instance" "this" {
+  ami                                  = "ami-0c1c30571d2dae5c9"
+  associate_public_ip_address          = true
+  instance_type                        = "t2.micro"
+  key_name                             = aws_key_pair.sample_key.key_name
+  vpc_security_group_ids               = [aws_security_group.this.id]
 }
 
 output "sample_server_dns" {
-  value = aws_instance.sample_server.public_dns
+  value = aws_instance.this.public_dns
 }
+
